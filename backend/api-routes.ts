f8bf68d9897b99a219f80e9483707ed141ddfb40
Tito Alvarez <augustoalvarez@gmail.com>
2023-07-04 23:38:24 -0600

    Health endpoint

--- backend/api-routes.ts
+++ backend/api-routes.ts
@@ -1,10 +1,39 @@
 import { Router } from "express";
+import * as fs from "fs";
 
 const router: Router = Router();
 
 // Set default API response
 router.get("/", function (req, res) {
   res.status(404).end();
+});
+
+// Health
+router.get("/health", function (req, res) {
+  // Read package.json file
+  fs.readFile("package.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(500).end();
+      return;
+    }
+
+    try {
+      // Parse package.json as JSON
+      const packageData = JSON.parse(data);
+
+      // Extract project name and version
+      const projectName = packageData.name;
+      const projectVersion = packageData.version;
+
+      res.send({
+        health: "Ok",
+        projectName: projectName,
+        projectVersion: projectVersion,
+      });
+    } catch (err) {
+      res.status(500).end();
+    }
+  });
 });
 
 module.exports = router;
