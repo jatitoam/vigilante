0d08c879bfa61dacd7f763283cefd74cc3d2b12a
Esdras Ajanel <esdras069@gmail.com>
2023-07-20 20:45:12 -0600

    Frontend Base Created

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from '../views/LoginView.vue'
+import Home from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/home',
       name: 'home',
-      component: HomeView
+      component: Home
     },
   ]
 })
